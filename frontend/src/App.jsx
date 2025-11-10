90cf7d1cdfd99b668979b8ed2555ede39cff8141
EderMoros3 <edermorosrodero@gmail.com>
2025-11-10 14:29:59 +0100

    feat: estilos de header (por hacer)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import DotGrid from "./components/DotGrid/DotGrid.jsx";
 import Header from "./components/Header/Header.jsx";
-function App() {
+import DotGrid from "./components/DotGrid/DotGrid.jsx";
+
+export default function App() {
   return (
-    <div className="relative w-full h-screen">
-    <div className="relative z-10">
-    <Header />
-    </div>
-      <div className="absolute inset-0 -z-10">
+    <div className="relative min-h-screen">
+      {/* Fondo global */}
+      <div className="fixed inset-0 -z-10">
         <DotGrid
           dotSize={10}
           gap={15}
@@ -21,11 +20,13 @@ function App() {
         />
       </div>
 
-      <div className="relative z-10 flex flex-col items-center justify-center h-full">
+      {/* Contenido */}
+      <Header />
+      <main className="flex flex-col items-center justify-center h-[200vh]">
         <h1 className="text-white text-4xl font-bold">Contenido encima</h1>
-      </div>
+      </main>
     </div>
   );
 }
 
-export default App;
+
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
-
 export default function Header() {
-  return(
-    <header className="w-full  flex justify-between border-2 border-red-700">
-      <div>
-        <p className="text-white text-3xl font-bold">Eder Moros</p>
-      </div>
-      <div>
-        <nav className="text-white text-2xl font-bold flex gap-1.5">
-          <a>Home</a>
-          <a>About</a>
-          <a>Portfolio</a>
-          <a>Contact</a>
-        </nav>
+  return (
+    <header className="w-full bg-transparent">
+      <div className="flex justify-between border border-gray-300 rounded-full bg-black/30 backdrop-blur-md 
+        pr-20 pl-20 pt-4 pb-4 mt-10 ml-10 mr-10 text-center">
+        <div>
+          <p className=" text-3xl font-bold text-transparent bg-clip-text bg-linear-to-r from-[#FAD4D8] to-[#FC440F]">
+            Eder Moros
+          </p>
+        </div>
+        <div>
+          <nav className="text-white text-2xl font-bold flex gap-2 ">
+            <a className="group-hover:text-red-400 hover:text-red-400">Home</a>
+            <a className="group-hover:text-red-400 hover:text-red-400">About</a>
+            <a className="group-hover:text-red-400 hover:text-red-400">
+              Portfolio
+            </a>
+            <a className="group-hover:text-red-400 hover:text-red-400">
+              Contact
+            </a>
+          </nav>
+        </div>
       </div>
     </header>
-  )
+  );
 }
